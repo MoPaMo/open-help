0f2bb14da47946362cfed12ff1225484cae8d6a3
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-06-28 19:14:23 +0200

    include "process" for env files

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const sqlite = require("sqlite3");
+const process = require("process");
 const path = require("path");
+const { env } = require("process");
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
