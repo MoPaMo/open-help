a734b227ef59dcd337daf9a9d18a3323a4ab5915
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-06-28 18:58:43 +0200

    Revert "add gitignore"
    This reverts commit f27ea145818259e0c47733ef65a5e5b7dbcb29ad.

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const sqlite3 = require("sqlite3").verbose();
-
 const path = require("path");
 
 app.get("/", (req, res) => {
