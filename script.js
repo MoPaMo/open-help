f27ea145818259e0c47733ef65a5e5b7dbcb29ad
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-06-28 18:58:03 +0200

    add gitignore

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const sqlite3 = require("sqlite3").verbose();
+
 const path = require("path");
 
 app.get("/", (req, res) => {
