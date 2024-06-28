ac669c37a0919a04dae4f51694244996a29685c5
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-06-28 18:18:27 +0200

    include path utility

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const path = require("path");
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
