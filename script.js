2c80bea2a47dead10921757b64e412083b9215c4
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-06-28 18:16:02 +0200

    add express scripts

---
+++ script.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// make all files under 'public' public
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
