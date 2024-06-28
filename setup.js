14c437dc8a8552f9fcd224ad9dab66167cd8ab7f
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-06-28 20:00:11 +0200

    create admin user with secure random password

--- setup.js
+++ setup.js
@@ -3,6 +3,12 @@ const process = require("process");
 const express = require("express");
 const crypto = require("crypto");
 
+function generateRandomString(length) {
+  return crypto
+    .randomBytes(Math.ceil(length / 2))
+    .toString("hex")
+    .slice(0, length);
+}
 const dbFile = "./db.sqlite3";
 const fs = require("fs");
 
@@ -17,10 +23,15 @@ if (!fs.existsSync(dbFile)) {
   console.log("Database file deleted.");
 }
 const db = new sqlite3.Database(dbFile);
-
+let pwd = generateRandomString(24);
 db.serialize(() => {
   db.run(
     "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)"
   );
+  db.run("INSERT INTO users (username, password) VALUES (?, ?)", [
+    "admin",
+    pwd,
+  ]);
 });
+console.log("Database created with admin password: " + pwd);
 db.close();
