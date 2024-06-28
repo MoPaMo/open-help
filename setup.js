af51874cde0c52ce004e3726016235679d98f6db
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-06-28 19:58:15 +0200

    create user database

--- setup.js
+++ setup.js
@@ -1,6 +1,7 @@
 const sqlite3 = require("sqlite3").verbose();
 const process = require("process");
 const express = require("express");
+const crypto = require("crypto");
 
 const dbFile = "./db.sqlite3";
 const fs = require("fs");
@@ -16,3 +17,10 @@ if (!fs.existsSync(dbFile)) {
   console.log("Database file deleted.");
 }
 const db = new sqlite3.Database(dbFile);
+
+db.serialize(() => {
+  db.run(
+    "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)"
+  );
+});
+db.close();
