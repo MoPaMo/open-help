729a827dd0693dbd810429280ac63b4012e423fa
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-06-28 19:17:04 +0200

    chore: Check if database file exists before starting the application

--- setup.js
+++ setup.js
@@ -1,3 +1,11 @@
 const sqlite3 = require("sqlite3").verbose();
 const process = require("process");
 const express = require("express");
+
+const dbFile = "./db.sqlite3";
+const fs = require("fs");
+
+if (!fs.existsSync(dbFile)) {
+  console.error("Database file does not exist.");
+  process.exit(1);
+}
