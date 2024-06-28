d721431c6a192d470194e995ea16f620364c303f
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-06-28 19:55:03 +0200

    create new db file in setup

--- setup.js
+++ setup.js
@@ -7,5 +7,12 @@ const fs = require("fs");
 
 if (!fs.existsSync(dbFile)) {
   console.error("Database file does not exist.");
-  process.exit(1);
+  //create file
+  fs.writeFileSync(dbFile, "");
+} else {
+  console.log("Database file already exists.");
+  //delete file
+  fs.unlinkSync(dbFile);
+  console.log("Database file deleted.");
 }
+const db = new sqlite3.Database(dbFile);
